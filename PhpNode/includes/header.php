566787ce81a2694d9992c0f8188266083e692105
ajlopez <webmaster@ajlopez.com>
2013-09-22 17:05:28 -0300

    Fractal at browser; Fractal with Node server

--- PhpNode/includes/header.php
+++ PhpNode/includes/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="js/jquery-2.0.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -33,6 +35,17 @@
                     </li>
                     <li role='presentation'>
                         <a role='menuitem' tabindex='-1' href='chat.php'>Chat</a>
+                    </li>
+                </ul>
+            </li>
+            <li class='dropdown'>
+                <a role='button' data-toggle='dropdown' href='#'>Fractal <span class="caret"></span></a>
+                <ul class='dropdown-menu'>
+                    <li role='presentation'>
+                        <a role='menuitem' tabindex='-1' href='fractal.php'>Browser</a>
+                    </li>
+                    <li role='presentation'>
+                        <a role='menuitem' tabindex='-1' href='fractalserver.php'>Server</a>
                     </li>
                 </ul>
             </li>
